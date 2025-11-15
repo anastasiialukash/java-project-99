5051efc71ffddb74ac2a391e65fcff6ec43ef361
Anastasia Lukash <anastasia16rom@gmail.com>
2025-11-15 23:26:24 +0100

    fix build

--- app/src/main/java/hexlet/code/AppApplication.java
+++ app/src/main/java/hexlet/code/AppApplication.java
@@ -2,12 +2,19 @@ package hexlet.code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+    @GetMapping("/")
+    String home() {
+        return "Hello!";
+    }
 }
