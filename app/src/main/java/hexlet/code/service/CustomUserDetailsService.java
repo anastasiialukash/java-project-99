efdbff5050e53b9e28e8236091f2ef2223df1070
Anastasia Lukash <anastasia16rom@gmail.com>
2025-12-23 23:04:58 +0100

    fix tests

--- app/src/main/java/hexlet/code/service/CustomUserDetailsService.java
+++ app/src/main/java/hexlet/code/service/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package hexlet.code.service;
 import hexlet.code.model.User;
 import hexlet.code.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Primary
 public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
